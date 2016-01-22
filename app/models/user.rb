c176717dc2c839961f1bbd8d6c13960f172b81f4
Scotty Runyan <runyans7@gmail.com>
2016-01-22 14:55:22 -0500

    Email validation on signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,25 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
 
+  validate :email_is_unique, on: :create
+  after_create :create_account
+
   def confirmation_required?
     false
+  end
+
+  private
+
+  def email_is_unique
+    return false unless self.errors[:email].empty?
+
+    unless Account.find_by_email(email).nil?
+      errors.add(:email, 'email is already taken')
+    end
+  end
+
+  def create_account
+    account = Account.new(email: email)
+    account.save!
   end
 end
